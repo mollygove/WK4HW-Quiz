8a1bcb14ae7df91fe5dc39b5dc33f870c894a6f3
mollygove <99143654+mollygove@users.noreply.github.com>
2022-03-27 13:25:40 -0400

    added HTML CSS and JS

---
+++ Assets/JS/score.js
@@ -0,0 +1,27 @@
+var olEl = document.querySelector("#nameScore");
+var highScoresList = [];
+
+console.log(highScoresList);
+
+function renderScore () {
+    olEl.innerHTML = "";
+    
+    for (var i = 0; i < highScoresList.length; i++) {
+        var highS = highScoresList[i];
+        var liEl = document.createElement("li");
+        liEl.textContent = `${highS.name} ${highS.score}`; 
+        liEl.setAttribute("data-index", i);
+        olEl.appendChild(liEl);
+    }
+};
+
+function init() {
+    var highScores = JSON.parse(localStorage.getItem("highScores"));
+    if (highScores !== null) {
+        highScoresList = highScores;
+    }
+    console.log(highScoresList);
+    renderScore();
+};
+
+init();
