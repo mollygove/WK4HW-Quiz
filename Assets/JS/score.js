3e8ada198604c709568cedc53c371becceaf5e36
mollygove <99143654+mollygove@users.noreply.github.com>
2022-03-29 18:33:17 -0400

    updating formatting

--- Assets/JS/score.js
+++ Assets/JS/score.js
@@ -3,25 +3,25 @@ var highScoresList = [];
 
 console.log(highScoresList);
 
-function renderScore () {
-    olEl.innerHTML = "";
-    
-    for (var i = 0; i < highScoresList.length; i++) {
-        var highS = highScoresList[i];
-        var liEl = document.createElement("li");
-        liEl.textContent = `${highS.name} ${highS.score}`; 
-        liEl.setAttribute("data-index", i);
-        olEl.appendChild(liEl);
-    }
-};
+function renderScore() {
+  olEl.innerHTML = "";
+
+  for (var i = 0; i < highScoresList.length; i++) {
+    var highS = highScoresList[i];
+    var liEl = document.createElement("li");
+    liEl.textContent = `${highS.name} ${highS.score}`;
+    liEl.setAttribute("data-index", i);
+    olEl.appendChild(liEl);
+  }
+}
 
 function init() {
-    var highScores = JSON.parse(localStorage.getItem("highScores"));
-    if (highScores !== null) {
-        highScoreList = highScores;
-    }
-    console.log(highScoresList);
-    renderScore();
-};
+  var highScores = JSON.parse(localStorage.getItem("highScores"));
+  if (highScores !== null) {
+    highScoreList = highScores;
+  }
+  console.log(highScoresList);
+  renderScore();
+}
 
 init();
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,107 +1,117 @@
 //Link elements by ID
 var startButton = document.querySelector("#startButton");
-var timerEl = document.querySelector('#timer');
+var timerEl = document.querySelector("#timer");
 
-var questionEl = document.querySelector('#question');
-var b1El = document.querySelector('#b1');
-var b2El = document.querySelector('#b2');
-var b3El = document.querySelector('#b3');
+var questionEl = document.querySelector("#question");
+var b1El = document.querySelector("#b1");
+var b2El = document.querySelector("#b2");
+var b3El = document.querySelector("#b3");
 
 var introEl = document.querySelector("#intro");
 var quizEl = document.querySelector("#quiz");
 
 //Questions and Answers
 var questions = [
-    {
-        question: "_____ is the standard markup language for Web pages",
-        q1: "Javascript",
-        q2: "HTML",
-        q3: "CSS",
-        answer: "HTML"
-    },
-    {
-        question: "HTML Tags are:",
-        q1: "<a>",
-        q2: "<button>",
-        q3: "All of the Above",
-        answer: "All of the Above"
-    },
-    {
-        question: "Global event attributes are added to HTML elements to define event actions.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "True"
-    },
-    {
-        question: "href specifies the title of a page.",
-        q1: "not this one",
-        q2: "False",
-        q3: "True",
-        answer: "False"
-    },
-    {
-        question: "<body> defines the document's body.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "True"
-    },
-    {
-        question: "CSS is the language we use to style an HTML document.",
-        q1: "True",
-        q2: "not this one",
-        q3: "False",
-        answer: "True"
-    },
-    {   question: "HTML describes how CSS elements should be displayed.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "False"
-    },
-    {   question: "border-style property specifies what kind of border to display.",
-        q1: "not this one",
-        q2: "False",
-        q3: "True",
-        answer: "True"
-    },
-    {   question: "A pseudo-class is used to define a special state of a property.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "False"
-    },
-    {   question: "JavaScript is the world's most popular programming language.",
-        q1: "not this one",
-        q2: "False",
-        q3: "True",
-        answer: "True"
-    },
-    {   question: "JavaScript can change the style of an HTML element.",
-        q1: "True",
-        q2: "not this one",
-        q3: "False",
-        answer: "True"
-    },
-    {   question: "Single line comments start with / in JavaScript.",
-        q1: "True",
-        q2: "not this one",
-        q3: "False",
-        answer: "False"
-    },
-    {   question: "The external JavaScript file must contain the <script> tag.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "False"
-    },
-    {   question: "JavaScript is the same as Java.",
-        q1: "True",
-        q2: "False",
-        q3: "not this one",
-        answer: "False"
-    }];
+  {
+    question: "_____ is the standard markup language for Web pages",
+    q1: "Javascript",
+    q2: "HTML",
+    q3: "CSS",
+    answer: "HTML",
+  },
+  {
+    question: "HTML Tags are:",
+    q1: "<a>",
+    q2: "<button>",
+    q3: "All of the Above",
+    answer: "All of the Above",
+  },
+  {
+    question:
+      "Global event attributes are added to HTML elements to define event actions.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "True",
+  },
+  {
+    question: "href specifies the title of a page.",
+    q1: "not this one",
+    q2: "False",
+    q3: "True",
+    answer: "False",
+  },
+  {
+    question: "<body> defines the document's body.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "True",
+  },
+  {
+    question: "CSS is the language we use to style an HTML document.",
+    q1: "True",
+    q2: "not this one",
+    q3: "False",
+    answer: "True",
+  },
+  {
+    question: "HTML describes how CSS elements should be displayed.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "False",
+  },
+  {
+    question: "border-style property specifies what kind of border to display.",
+    q1: "not this one",
+    q2: "False",
+    q3: "True",
+    answer: "True",
+  },
+  {
+    question: "A pseudo-class is used to define a special state of a property.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "False",
+  },
+  {
+    question: "JavaScript is the world's most popular programming language.",
+    q1: "not this one",
+    q2: "False",
+    q3: "True",
+    answer: "True",
+  },
+  {
+    question: "JavaScript can change the style of an HTML element.",
+    q1: "True",
+    q2: "not this one",
+    q3: "False",
+    answer: "True",
+  },
+  {
+    question: "Single line comments start with / in JavaScript.",
+    q1: "True",
+    q2: "not this one",
+    q3: "False",
+    answer: "False",
+  },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "False",
+  },
+  {
+    question: "JavaScript is the same as Java.",
+    q1: "True",
+    q2: "False",
+    q3: "not this one",
+    answer: "False",
+  },
+];
 
 //Initial variables
 var userScore;
@@ -111,78 +121,77 @@ var timeInt;
 
 //Start the timer
 function startTimer() {
-    timeInt = setInterval(
-        function () {
-            secondsLeft--;
-            timerEl.textContent = `Timer: ${secondsLeft}`;
-            if (secondsLeft === 0) {
-                userScore = 0;
-                clearInterval(timeInt);
-                timerEl.textContent = " ";
-                alert("Times Up!");
-                userScore = 0;
-                enterHiSc();
-            }
-        }, 1000);
-};
+  timeInt = setInterval(function () {
+    secondsLeft--;
+    timerEl.textContent = `Timer: ${secondsLeft}`;
+    if (secondsLeft === 0) {
+      userScore = 0;
+      clearInterval(timeInt);
+      timerEl.textContent = " ";
+      alert("Times Up!");
+      userScore = 0;
+      enterHiSc();
+    }
+  }, 1000);
+}
 
-//Start the game 
+//Start the game
 startButton.addEventListener("click", startQuiz);
 function startQuiz() {
-    userScore = 0;
-    startTimer();
-    introEl.setAttribute("style", "display: none");
-    quizEl.setAttribute("style", "display: block");
-    loadQuestions();
-};
+  userScore = 0;
+  startTimer();
+  introEl.setAttribute("style", "display: none");
+  quizEl.setAttribute("style", "display: block");
+  loadQuestions();
+}
 
 //Load the questions based on index
 function loadQuestions() {
-    questionEl.textContent = questions[questionIndex].question;
-    b1El.textContent = `${questions[questionIndex].q1}`;
-    b2El.textContent = `${questions[questionIndex].q2}`;
-    b3El.textContent = `${questions[questionIndex].q3}`;
-    };
+  questionEl.textContent = questions[questionIndex].question;
+  b1El.textContent = `${questions[questionIndex].q1}`;
+  b2El.textContent = `${questions[questionIndex].q2}`;
+  b3El.textContent = `${questions[questionIndex].q3}`;
+}
 
 //Validate users choices
 var wrongEl = document.querySelector("#wrong");
 quizEl.addEventListener("click", function (event) {
-    var element = event.target;
-    if (element.matches(".quizB")) {
-        var check = element.innerText;
-        if (check === questions[questionIndex].answer) {
-            secondsLeft = secondsLeft + 5;
-            alert("Correct!");
-            wrongEl.textContent = " ";
-            //Run through questions
-            var qLength = questions.length - 1;
-            if (questionIndex < qLength) {
-                questionIndex++;
-                loadQuestions();
-            } else {
-                //Ran through all questions - finish
-                alert("All Done!");
-                userScore = secondsLeft;
-                clearInterval(timeInt);
-                timerEl.textContent = " ";
-                enterHiSc();
-            }
-        } else {
-            secondsLeft = secondsLeft - 5;
-            wrongEl.textContent = "Incorrect -5 seconds";
-            if (secondsLeft <= 0) {
-                userScore = 0;
-                clearInterval(timeInt);
-                timerEl.textContent = " ";
-                alert("Ran out of time!");
-                enterHiSc();
-            }
-        }
+  var element = event.target;
+  if (element.matches(".quizB")) {
+    var check = element.innerText;
+    if (check === questions[questionIndex].answer) {
+      secondsLeft = secondsLeft + 5;
+      alert("Correct!");
+      wrongEl.textContent = " ";
+      //Run through questions
+      var qLength = questions.length - 1;
+      if (questionIndex < qLength) {
+        questionIndex++;
+        loadQuestions();
+      } else {
+        //Ran through all questions - finish
+        alert("All Done!");
+        userScore = secondsLeft;
+        clearInterval(timeInt);
+        timerEl.textContent = " ";
+        enterHiSc();
+      }
+    } else {
+      secondsLeft = secondsLeft - 5;
+      wrongEl.textContent = "Incorrect -5 seconds";
+      if (secondsLeft <= 0) {
+        userScore = 0;
+        clearInterval(timeInt);
+        timerEl.textContent = " ";
+        alert("Ran out of time!");
+        enterHiSc();
+      }
     }
+  }
 });
 
 //Elements in JS for enter score section.
-var mainEl = document.querySelector('main');
+var mainEl = document.querySelector("main");
 var sectionEl = document.createElement("section");
 var h1El = document.createElement("h1");
 var h2El = document.createElement("h2");
@@ -191,40 +200,40 @@ var inputEl = document.createElement("input");
 var buttonEl = document.createElement("button");
 
 function enterHiSc() {
-    quizEl.setAttribute("style", "display: none");
+  quizEl.setAttribute("style", "display: none");
 
-    mainEl.appendChild(sectionEl);
-    sectionEl.appendChild(h1El);
-    sectionEl.appendChild(h2El);
-    sectionEl.appendChild(labelEl);
-    sectionEl.appendChild(inputEl);
-    sectionEl.appendChild(buttonEl);
+  mainEl.appendChild(sectionEl);
+  sectionEl.appendChild(h1El);
+  sectionEl.appendChild(h2El);
+  sectionEl.appendChild(labelEl);
+  sectionEl.appendChild(inputEl);
+  sectionEl.appendChild(buttonEl);
 
-    sectionEl.setAttribute("class", "sectionEl");
-    h1El.setAttribute("class", "h1El");
-    h2El.setAttribute("class", "h2El");
-    labelEl.setAttribute("class", "labelEl");
-    inputEl.setAttribute("class", "inputEl");
-    inputEl.setAttribute("placeholder", "...");
-    buttonEl.setAttribute("class", "buttonEl");
+  sectionEl.setAttribute("class", "sectionEl");
+  h1El.setAttribute("class", "h1El");
+  h2El.setAttribute("class", "h2El");
+  labelEl.setAttribute("class", "labelEl");
+  inputEl.setAttribute("class", "inputEl");
+  inputEl.setAttribute("placeholder", "...");
+  buttonEl.setAttribute("class", "buttonEl");
 
-    h1El.textContent = "Thanks for playing!";
-    h2El.textContent = `Your score is ${userScore}.`;
-    labelEl.textContent = "Please enter your initials: ";
-    buttonEl.textContent = "Post";
+  h1El.textContent = "Thanks for playing!";
+  h2El.textContent = `Your score is ${userScore}.`;
+  labelEl.textContent = "Please enter your initials: ";
+  buttonEl.textContent = "Post";
 
-	//Save values to array
-	buttonEl.addEventListener("click", function (event) {
-		event.preventDefault();
-		var highScore =JSON.parse(localStorage.getItem("highScores")) || [];
+  //Save values to array
+  buttonEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    var highScore = JSON.parse(localStorage.getItem("highScores")) || [];
 
-		var highScores = {
-			name: inputEl.value.trim(),
-			score: userScore
-		};
+    var highScores = {
+      name: inputEl.value.trim(),
+      score: userScore,
+    };
 
-		highScore.push(highScores);
+    highScore.push(highScores);
 
-		localStorage.setItem("highScores", JSON.stringify(highScore));
-	});
-};
+    localStorage.setItem("highScores", JSON.stringify(highScore));
+  });
+}
